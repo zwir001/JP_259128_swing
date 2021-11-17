dc4968fbbab75b3fbf4c621be0d2b03b2e696610
zwir001 <259128@student.pwr.edu.pl>
2021-11-17 18:17:36 +0100

    Działający przycisk logowania

--- src/tb/soft/Window.java
+++ src/tb/soft/Window.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Window extends JFrame {
 
-    private final Set<User> user_data = new HashSet<>();
+    private Map<String, String> user_data = new HashMap<>();
 
     public Window() throws HeadlessException {
         this("Undefined");
@@ -24,27 +24,63 @@ public class Window extends JFrame {
     private void build_UI(){
         JPanel panel= new JPanel();
         add(panel);
-
+        GridBagConstraints c = new GridBagConstraints();
         setBounds(450, 300, 600, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel log_in_label = new JLabel("Login: ");
+        set_element(panel, log_in_label, 0, 0);
+
+        JTextField name = new JTextField(10);
+        set_element(panel, name, 1, 0);
+
+        JLabel password = new JLabel("Hasło: ");
+        set_element(panel, password, 0, 1);
+
+        JPasswordField key = new JPasswordField(10);
+        set_element(panel, key, 1, 1);
 
         JButton log_in = new JButton("Zaloguj");
+        JButton cancel = new JButton("Cancel");
 
         log_in.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                check_data(name, key, panel);
+            }
+        });
+        //log_in.setBounds(20,200,90, 23);
+        set_element(panel, log_in,0, 2);
+        cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
+        set_element(panel, cancel, 1, 2);
     }
 
     private void add_user(String name, String password){
-        User user=new User(name, password);
-        this.user_data.add(user);
+        this.user_data.put(name, password);
     }
 
+    private void set_element(JPanel panel, JComponent element, int x, int y){
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = x;
+        c.gridy = y;
+        panel.add(element);
+    }
+
+    private void check_data( JTextField name, JPasswordField password, JPanel panel ) {
+            String login = name.getText();
+            String key = new String(password.getPassword());
+            if(user_data.containsKey(key) && user_data.containsValue(login)){
+            panel.setBackground(Color.green);
+            }
+            else{
+            panel.setBackground(Color.red);
+            }
+    }
     private void test_users(){
         add_user("student", "student");
         add_user("teacher", "1234");
